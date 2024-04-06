81baeb945a0dfcbaee7653e584ba063171f57e7b
Basar Buyukkahraman <basarbk@yahoo.com>
2024-04-06 22:58:05 +0300

    0204-progress-n-success

--- src/pages/sign-up/index.jsx
+++ src/pages/sign-up/index.jsx
@@ -1,20 +1,31 @@
-import { createSignal } from "solid-js";
+import { Show, createSignal } from "solid-js";
 export function SignUp() {
   const [email, setEmail] = createSignal();
+  const [apiProgress, setApiProgress] = createSignal(false);
+  const [successMessage, setSuccessMessage] = createSignal();
 
   const onInputEmail = (event) => {
     setEmail(event.target.value);
   };
 
-  const submit = (event) => {
+  const submit = async (event) => {
     event.preventDefault();
-    fetch("/api/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email: email() }),
-    });
+    setApiProgress(true);
+    setSuccessMessage();
+    try {
+      const response = await fetch("/api/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: email() }),
+      });
+      const body = await response.json();
+      setSuccessMessage(body.message);
+    } catch {
+    } finally {
+      setApiProgress(false);
+    }
   };
 
   return (
@@ -30,8 +41,22 @@ export function SignUp() {
             </label>
             <input id="email" class="form-control" onInput={onInputEmail} />
           </div>
+          <Show when={successMessage()}>
+            <div class="alert alert-success" role="alert">
+              {successMessage()}
+            </div>
+          </Show>
           <div class="text-center">
-            <button disabled={!email()} class="btn btn-primary">
+            <button
+              disabled={!email() || apiProgress()}
+              class="btn btn-primary"
+            >
+              <Show when={apiProgress()}>
+                <span
+                  class="spinner-border spinner-border-sm"
+                  aria-hidden="true"
+                ></span>
+              </Show>
               Sign Up
             </button>
           </div>
