b3d056baec16de2d60de3d0abf52f3794698459f
Basar Buyukkahraman <basarbk@yahoo.com>
2024-04-06 21:50:10 +0300

    0201-sign-up-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
-import logo from "./logo.svg";
-import styles from "./App.module.css";
+import { SignUp } from "./pages/sign-up";
 
 function App() {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+  return <SignUp />;
 }
 
 export default App;
---
+++ src/pages/sign-up/index.jsx
@@ -0,0 +1,10 @@
+export function SignUp() {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <label for="email">Email</label>
+      <input id="email" />
+      <button disabled>Sign Up</button>
+    </div>
+  );
+}
