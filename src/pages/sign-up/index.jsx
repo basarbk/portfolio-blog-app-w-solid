c092594b36f5b59e25d5bf2040b4b98287305852
Basar Buyukkahraman <basarbk@yahoo.com>
2024-04-06 22:13:10 +0300

    0202-submit

--- src/pages/sign-up/index.jsx
+++ src/pages/sign-up/index.jsx
@@ -1,10 +1,28 @@
+import { createSignal } from "solid-js";
 export function SignUp() {
+  const [email, setEmail] = createSignal();
+
+  const onInputEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const submit = (event) => {
+    event.preventDefault();
+    fetch("/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email() }),
+    });
+  };
+
   return (
-    <div>
+    <form onSubmit={submit}>
       <h1>Sign Up</h1>
       <label for="email">Email</label>
-      <input id="email" />
-      <button disabled>Sign Up</button>
-    </div>
+      <input id="email" onInput={onInputEmail} />
+      <button disabled={!email()}>Sign Up</button>
+    </form>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,9 @@ export default defineConfig({
   ],
   server: {
     port: 5173,
+    proxy: {
+      "/api": "http://localhost:8080",
+    },
   },
   build: {
     target: "esnext",
