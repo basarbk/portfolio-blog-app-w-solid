a660b480e77d22b13e397a20041289f8df67d44b
Basar Buyukkahraman <basarbk@yahoo.com>
2024-04-10 21:42:41 +0300

    0604-suspense-n-error-boundary

--- src/pages/article/view/index.jsx
+++ src/pages/article/view/index.jsx
@@ -1,5 +1,5 @@
-import { Match, Switch, createResource } from "solid-js";
-import { useParams } from "@solidjs/router";
+import { ErrorBoundary, Suspense, createResource } from "solid-js";
+import { useIsRouting, useParams } from "@solidjs/router";
 import { AppAlert, AppSpinner } from "../../../components";
 
 const fetchArticle = async (id) => {
@@ -11,23 +11,30 @@ const fetchArticle = async (id) => {
 
 export function ArticleView() {
   const params = useParams();
+  const isRouting = useIsRouting();
 
   const [article] = createResource(() => params.idOrSlug, fetchArticle);
 
   return (
-    <Switch>
-      <Match when={article.loading}>
-        <AppSpinner full size="regular" />
-      </Match>
-      <Match when={article.error}>
-        <AppAlert variant="danger">{article.error.message}</AppAlert>
-      </Match>
-      <Match when={article()}>
-        <main class="bg-white border rounded py-3 px-5">
-          <h1 class="text-capitalize">{article().title}</h1>
-          <div>{article().content}</div>
-        </main>
-      </Match>
-    </Switch>
+    <Suspense fallback={<AppSpinner full size="regular" />}>
+      <ErrorBoundary
+        fallback={(error) => (
+          <AppAlert variant="danger">{error.message}</AppAlert>
+        )}
+      >
+        <div class="placeholder-glow">
+          <div
+            classList={{
+              "placeholder rounded w-100": isRouting(),
+            }}
+          >
+            <main class="bg-white border rounded py-3 px-5">
+              <h1 class="text-capitalize">{article()?.title}</h1>
+              <div>{article()?.content}</div>
+            </main>
+          </div>
+        </div>
+      </ErrorBoundary>
+    </Suspense>
   );
 }
